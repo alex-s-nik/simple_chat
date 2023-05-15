b0e743eaefe65d913365e6277e9ec200eb705f51
alex-s-nik <to_ctrl@mail.ru>
2023-05-15 22:27:27 +0300

    docs: fill readme with more info

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
-NUMBER_OF_HISTORY_MESSAGES = 4
+NUMBER_OF_HISTORY_MESSAGES = 20
 TIME_TO_BAN = 60
